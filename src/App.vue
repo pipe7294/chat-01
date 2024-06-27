657372824ab70712731507654037c03567d85867
Felipe Carmona <felipecarmonamaturana@gmail.com>
2024-06-27 01:26:33 -0400

    chat en vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,72 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <!-- primer usuario  -->
+    <div class="usuario">
+      <img :src="usuario1.foto" alt="foto usuario 1">
+      <span>{{ usuario1.nombre }}</span>
+      <input v-model="usuario1.color" type="color">
+      <textarea v-model="usuario1.textoMensaje" name="" id=""></textarea>
+      <button @click="enviarMensaje(usuario1)">Enviar</button>
+    </div>
+
+
+    <!-- segundo usuario  -->
+    <div class="usuario">
+      <img :src="usuario2.foto" alt="foto usuario 2">
+      <span>{{ usuario2.nombre }}</span>
+      <input v-model="usuario2.color" type="color">
+      <textarea v-model="usuario2.textoMensaje" name="" id=""></textarea>
+      <button @click="enviarMensaje(usuario2)">Enviar</button>
+    </div>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import chatUsuario from './components/chatUsuario.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    chatUsuario,
+  },
+  data(){
+    return {
+      usuario1:{
+        nombre:"",
+        foto:"",
+        textoMensaje:"",
+        color:"00FF00",
+        id:"0",
+      },
+      usuario2:{
+        nombre:"",
+        foto:"",
+        textoMensaje:"",
+        color:"D0FF00",
+        id:"1",
+      },
+      mensajesUsuarios:"",
+    }
+  },
+  methods: {
+    enviarMensaje(usuario){
+      this.mensajesUsuarios.push({...usuario})
+    }
+
+  },
+  async mounted(){
+    const url ="https://randomuser.me/api?results=2";
+    const {data} = await axios.get(url);
+
+    // usuario 1
+    this.usuario1.nombre = data.results[0].name.first;
+    this.usuario1.foto = data.results[0].picture.large;
+
+    // usuario 2
+    this.usuario2.nombre = data.results[1].name.first;
+    this.usuario2.foto = data.results[1].picture.large;
   }
 }
 </script>
@@ -22,5 +79,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.usuario{
+  display: flex;
+  flex-direction: column;
+}
+
+input {
+  width: 100%;
 }
 </style>
---
+++ src/components/chatUsuario.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="chat-box">
+        <div v-for="(mensaje, i) in mensajes" :key="i" 
+        :class="{izquierda: mensaje.id == 0, derecha: mensaje.id == 1}">
+            <p>{{ mensaje.nombre }}</p>
+            <p :style= "{backgroundColor: mensaje.color}">{{ mensaje.textoMensaje }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props:["mensajes"],
+}
+
+</script>
+
+<style>
+
+.izquierda {
+    align-self: flex-start;
+    text-align: left;
+    align-items: flex-start;
+}
+.derecha {
+    align-self: flex-start;
+    text-align: right;
+    align-items: flex-start;
+}
+
+</style>
